cd54ae52d5e0641c357e26af3a1015298ee03474
Andrew Davey <ajdavey8@gmail.com>
2018-01-15 22:48:59 +0000

    Passes test to add dog

--- test/integration/apiTest.js
+++ test/integration/apiTest.js
@@ -1,14 +1,29 @@
 process.env.NODE_ENV = 'test';
-import db from '../../server/models/index.js'
 const supertest = require('supertest');
 const should = require('chai').should;
 const expect = require('chai').expect;
 const app = require('../../app.js')
-console.log(typeof(db))
-describe('/GET users', function() {
-  after(function(done) {
-    let server = app.listen(3001)
-    server.close(done);
+
+describe('Users', function() {
+  it('should POST a new user', function(done) {
+    supertest(app)
+    .post('/signup')
+    .set('Accept', 'application/x-www-form-urlencoded')
+    .send({
+      firstname: "Foo",
+      lastname: "Bar",
+      email: "test53@gmail.com",
+      username: "Foo",
+      password: "123456",
+      city: "London",
+      postcode: "E11",
+      type: "Owner"
+    })
+    .expect(201)
+    .end(function(err,res) {
+      expect({success:true});
+      done();
+    });
   });
   it('should GET a list of users', function(done) {
     supertest(app)
@@ -29,40 +44,19 @@ describe('/GET users', function() {
   });
 });
 
-  it('should POST a new user', function(done) {
+describe('Dogs', function() {
+  it('should POST a new Dog', function(done) {
     supertest(app)
-    .post('/signup')
-    .set('Accept', 'application/x-www-form-urlencoded')
-    .send({
-      firstname: "Foo",
-      lastname: "Bar",
-      email: "test53@gmail.com",
-      username: "Foo",
-      password: "123456",
-      city: "London",
-      postcode: "E11",
-      type: "Owner"
-    })
+    .post('/api/users/1/dogs')
+    .set('Accept', 'multipart/form-data')
+    .field('name', 'Max')
+    .field('breed', 'Dobermann')
+    .field('description', 'test dog')
+    .attach('file', './public/dobermann.jpg')
     .expect(201)
     .end(function(err,res) {
       expect({success:true});
       done();
     });
-});
-
-  it('should POST a new Dog', function(done) {
-    supertest(app)
-    .post('/api/users/1/dogs')
-    .set('Accept', 'application/form-data')
-    .send({
-      name: "Dog",
-      breed: "Golden Retriever",
-      description: "test dog",
-      file: '../../public/dobermann.jpg'
-    })
-    .expect(201)
-    .end(function(err,res) {
-      expect({success:true});
-      done()
-    });
+  });
 });
