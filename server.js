55d3cae895bcabf772e83ec15f54e9367035461e
Andrew Davey <ajdavey8@gmail.com>
2018-01-16 16:20:03 +0000

    Fix typo

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ io.on('connection', (socket) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 const passport = require('passport');
