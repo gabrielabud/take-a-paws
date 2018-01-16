37b350478c2b37ea896b6d3aa0b8a9db5f289448
Andrew Davey <ajdavey8@gmail.com>
2018-01-16 19:00:35 +0000

    Send index.html and use build folder for all environments

--- server.js
+++ server.js
@@ -16,10 +16,11 @@ io.on('connection', (socket) => {
   })
 });
 
-if (process.env.NODE_ENV === "production") {
-  console.log(path)
-app.use(express.static(path.join(__dirname, 'client/build')));
-}
+// if (process.env.NODE_ENV === "production") {
+  // app.use(express.static(path.join(__dirname, '/client/build/')));
+// } else {
+  app.use(express.static(path.join(__dirname, 'client/build/')));
+// }
 
 const passport = require('passport');
 const session = require('express-session');
@@ -75,8 +76,12 @@ app.get('/error', (req, res) => res.status(200).send({
   message: "404"
 }));
 
-app.get('/', (req, res) => res.status(200).send({
-  message: "200"
-}));
+// app.get('/', (req, res) => res.status(200).send({
+//   message: "200"
+// }));
+
+app.get('/', function(req, res) {
+  res.sendFile('index.html');
+})
 
 module.exports = app;
